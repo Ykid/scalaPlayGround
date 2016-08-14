57eefd731395c74ae66ad31bf04b4bca6307d1ed
david <vagercai@gmail.com>
2016-08-14 15:00:06 +0800

    update source to only show log future code

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,16 +1,8 @@
 
-import java.net.URL
-import java.util.concurrent.atomic.AtomicInteger
-
-import jp.co.bizreach.s3scala.S3
-import awscala.s3._
-import awscala.Region
-
-import scala.collection.mutable
-import scala.concurrent.{Await, Future, Promise}
-import scala.util.{Failure, Success, Try}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future, Promise}
+import scala.util.{Failure, Success}
 
 
 object Test extends App{
@@ -31,68 +23,26 @@ object Test extends App{
     "f3 done"
   }
 
-  def allResolved(futures: List[Future[_]]):Future[List[Any]] = {
-    val promise = Promise[List[Any]]
-    @volatile var results = new mutable.MutableList[(Int, Any)]()
-    for ((future, index) <- futures.zipWithIndex) {
-      future onComplete  {
-        case Failure(e) => promise.failure(e)
-        case Success(result) =>
-          results+=((index, result))
-          if (results.length == futures.length) {
-            val (_, innerResults) = results.sortBy(_._1).unzip
-            promise.trySuccess(innerResults.toList)
-          }
+  def logFuture[T](future: Future[T]):Future[T] = {
+    val promise = Promise[T]
+    future onComplete {
+      case Failure(e) => {
+        println(e.getMessage)
+        promise.tryFailure(e)
+      }
+      case Success(result) => {
+        println(result)
+        promise.trySuccess(result)
       }
     }
     promise.future
   }
 
-  def allResolved2(futures: List[Future[_]]):Future[List[Any]] = {
-    val promise = Promise[List[Any]]
-    var results = new mutable.MutableList[(Int, Any)]()
-    for ((future, index) <- futures.zipWithIndex) {
-      future onComplete  {
-        case Failure(e) => promise.failure(e)
-        case Success(result) =>
-          synchronized {
-            results+=((index, result))
-            if (results.length == futures.length) {
-              val (_, innerResults) = results.sortBy(_._1).unzip
-              promise.trySuccess(innerResults.toList)
-            }
-          }
-      }
-    }
-    promise.future
-  }
-
-  val f = allResolved2(List(f3,f2,f1))
-  f onComplete {
-    case Success(results) => results foreach println
-    case Failure(e) => println(s"error ${e.getLocalizedMessage}")
-  }
-
-//  def logFuture[T](future: Future[T]):Future[T] = {
-//    val promise = Promise[T]
-//    future onComplete {
-//      case Failure(e) => {
-//        println(e.getMessage)
-//        promise.tryFailure(e)
-//      }
-//      case Success(result) => {
-//        println(result)
-//        promise.trySuccess(result)
-//      }
-//    }
-//    promise.future
-//  }
-
-//  for {
-//    _ <- logFuture(f3)
-//    _ <- logFuture(f2)
-//    _ <- logFuture(f1)
-//  } yield ()
+  for {
+    _ <- logFuture(f3)
+    _ <- logFuture(f2)
+    _ <- logFuture(f1)
+  } yield ()
 
   val doneDie = Future {
     Thread.sleep(10000)
